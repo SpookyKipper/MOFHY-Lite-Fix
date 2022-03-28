e9ded8c2b3646fc22514446bc158dd209d6f92d0
Loading.. Please Wait <69704016+ImLoadingUuU@users.noreply.github.com>
2022-03-28 20:43:46 +0800

    Switch To PDO

--- src/includes/Connect.php
+++ src/includes/Connect.php
@@ -4,15 +4,20 @@ if(session_status() == PHP_SESSION_NONE){
 	session_start();
 }
 require __DIR__."/../modules/Database/Config.php";
+try {
+	$pdo = new PDO("mysql:host=$DataBase['hostname'];dbname=$DataBase['name']", $DataBase['username'], $DataBase['password'],); //Connect MySQL Using PDO
+        catch (PDOException $e) {
+  echo "Connection not established".$e->getMessage(); //Detect Faild Connect
+}
 if(!isset($connect)){
 	$connect = mysqli_connect(
 		$DataBase['hostname'],
 		$DataBase['username'],
 		$DataBase['password'],
 		$DataBase['name']
-	);
-	if(!$connect){
-		echo 'Connection not established'; 
-	} 
+	); //This Should Be Remove Later :) If Remove It Now,It Will Not Work!
+
+
 }
+
 ?>
