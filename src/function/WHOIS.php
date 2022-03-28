56e2840e4a153e6baf517911cec28acc3c7fcae2
Loading.. Please Wait <69704016+ImLoadingUuU@users.noreply.github.com>
2022-03-28 18:47:14 +0800

    XSS Fix

--- src/function/WHOIS.php
+++ src/function/WHOIS.php
@@ -3,6 +3,7 @@ include __DIR__.'/../modules/whois/whois.php';
 if(isset($_POST['domain'])){ 
 	$domain = $_POST['domain'];	
 	$domain = trim($domain);
+	$domain = htmlspecialchars($domain);
 	if(substr(strtolower($domain), 0, 7) == "http://") $domain = substr($domain, 7);
 	if(substr(strtolower($domain), 0, 8) == "https://") $domain = substr($domain, 8);
 	if(substr(strtolower($domain), 0, 4) == "www.") $domain = substr($domain, 4);
